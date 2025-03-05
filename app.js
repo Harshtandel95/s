03c3fa825c39027bb0c1d7e0cfd63c038895b7d4
rushabh ramani <rushabhramani16@gmail.com>
2025-03-05 06:56:09 +0530

    u

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 // Improved CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://c-dusky-phi.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
   optionsSuccessStatus: 200
